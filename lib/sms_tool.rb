d750ac26c27e721a92796a277ce5464b2feaf6ef
Louis Randall <louisrandall@Louiss-MacBook-Pro.local>
2017-09-20 20:02:19 -0700

    integreated live connections with twilio

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,16 @@
 module SmsTool
-	def self.send_sms(number:, message:)
-		puts "Sending SMS..."
-		puts "#{message} to #{number}"
-	end
+	account_sid = 'AC3933b5adbea227b7536de9d0112ba841'
+	auth_token = '729c0f3b283de6f7394ecfd78ad40f0e'
+
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
+  def self.send_sms(number:, message:)
+    @client.messages.create(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: "+1#{number}",
+      body: "#{message}"
+    )
+  end
 end
+
+SmsTool.send_sms(number: '4153048935', message: 'Here we go!')
