763911d18bcd1c33a794b06880023fff217306d1
Louis Randall <louisrandall@Louiss-MacBook-Pro.local>
2017-09-28 20:56:13 -0700

    brought in live data to the admin homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
 	def homepage
-		
+		@pending_approvals = Post.all.where(status: 'submitted')
+		@pending_audit_logs = AuditLog.all.where(stauts: 'pending')
 	end
 end
